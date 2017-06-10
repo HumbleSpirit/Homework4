817d69e7d25fa48f8119f5778337a59dcb50fc9d
HumbleSpirit <borodkin@gmail.com>
2017-06-10 15:22:44 +0300

    4th task

---
+++ src/homework4/Homework4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework4;
+
+/**
+ *
+ * @author sergey
+ */
+public class Homework4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final double pound = 0.4059;   
+        double pounds_in_kilo = 0;
+        pounds_in_kilo = 1/pound;
+        System.out.println(pounds_in_kilo);
+    }
+    
+}
