6a95a8078253ab70817b26ca0892285a13949450
HumbleSpirit <borodkin@gmail.com>
2017-06-12 13:08:22 +0300

    2nd task

--- src/homework4/Homework4.java
+++ src/homework4/Homework4.java
@@ -12,11 +12,22 @@ import java.util.Scanner;
  * @author sergey
  */
 public class Homework4 {
+    
+    public static void drawStars() throws InterruptedException {
+        // Cool ending of task with running asterisks
+        char charDraw = '*';
+        for (int i = 0; i < 40; i++) 
+        {
+         System.out.print(charDraw);
+         Thread.sleep(250);
+        }
+        System.out.println(charDraw);
+        };
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         
         int firstValue = 0;
         int secondValue = 0;
@@ -27,7 +38,7 @@ public class Homework4 {
         System.out.println("Find lesser value from two numbers");
         System.out.println("Please enter first number");
         firstValue = in.nextInt();  
-        System.out.println("Please enter first number");
+        System.out.println("Please enter second number");
         secondValue = in.nextInt();  
         if (firstValue < secondValue)
         {
@@ -41,29 +52,31 @@ public class Homework4 {
         {
             System.out.println("Values are equal");
         }
+        drawStars();
               
         System.out.println("Task 2");
-        System.out.println("Find lesser value from two numbers");
+        System.out.println("Find lesser value from three numbers");
         System.out.println("Please enter first number");
         firstValue = in.nextInt();  
-        System.out.println("Please enter first number");
+        System.out.println("Please enter second number");
         secondValue = in.nextInt();  
-        if (firstValue < secondValue)
-        {
-            System.out.println("First value lesser than second");
-        }
-        else if (firstValue > secondValue)
-        {
-            System.out.println("First value bigger than second");
-        }
-        else 
-        {
-            System.out.println("Values are equal");
-        }
+        System.out.println("Please enter third number");
+        thirdValue = in.nextInt();  
         
-
-        // TODO code application logic here
- 
+        if (firstValue < secondValue && firstValue < thirdValue)
+        {
+            System.out.println("First value is smallest");
+        }
+        else if (secondValue < thirdValue)
+        {
+            System.out.println("Second value is smallest");
+        }
+        else
+        {
+            System.out.println("Third value is smallest");
+        }
+        drawStars();
+    
     }
     
 }
