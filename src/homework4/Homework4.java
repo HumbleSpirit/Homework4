43ef47b5a10f6273308f6aa0dcbb855289695c7b
HumbleSpirit <borodkin@gmail.com>
2017-06-12 12:55:57 +0300

    1st task

--- src/homework4/Homework4.java
+++ src/homework4/Homework4.java
@@ -5,6 +5,8 @@
  */
 package homework4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sergey
@@ -15,6 +17,51 @@ public class Homework4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        int firstValue = 0;
+        int secondValue = 0;
+        int thirdValue = 0;
+        
+        Scanner in = new Scanner(System.in); 
+        System.out.println("Task 1");
+        System.out.println("Find lesser value from two numbers");
+        System.out.println("Please enter first number");
+        firstValue = in.nextInt();  
+        System.out.println("Please enter first number");
+        secondValue = in.nextInt();  
+        if (firstValue < secondValue)
+        {
+            System.out.println("First value lesser than second");
+        }
+        else if (firstValue > secondValue)
+        {
+            System.out.println("First value bigger than second");
+        }
+        else 
+        {
+            System.out.println("Values are equal");
+        }
+              
+        System.out.println("Task 2");
+        System.out.println("Find lesser value from two numbers");
+        System.out.println("Please enter first number");
+        firstValue = in.nextInt();  
+        System.out.println("Please enter first number");
+        secondValue = in.nextInt();  
+        if (firstValue < secondValue)
+        {
+            System.out.println("First value lesser than second");
+        }
+        else if (firstValue > secondValue)
+        {
+            System.out.println("First value bigger than second");
+        }
+        else 
+        {
+            System.out.println("Values are equal");
+        }
+        
+
         // TODO code application logic here
  
     }
